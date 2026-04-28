6a887fcd5b2461fde67c53d826d153fcc2092de0
Mohamed Yousif Idris <mohammedyousifstan@gmail.com>
2026-04-28 11:04:49 +0300

    Fix formatting by removing extra newline
    Removed unnecessary newline at the end of the file.

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
       },
     },
   },
-
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
